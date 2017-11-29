ed9cfcdfcdf5b3329723f1b392320a01403ce70b
davide <davide.stasio@gmail.com>
2017-11-29 12:01:48 +0100

    Basic operations

--- python_testing/python_testing.py
+++ python_testing/python_testing.py
@@ -1,9 +1,12 @@
 
 def parse(eq = str, returnString = False):
     eq = processParenthesis(eq)
+
+    result = processOperations(eq)
     if returnString:
-        return 'placeholder'
-    return eq
+        return str(result)
+    else:
+        return result
 
 def extractParenthesis(eq):
     startPos = -1
@@ -38,7 +41,49 @@ def processParenthesis(eq):
                    1)
     return eq
 
+def processOperations(eq):
+    eq = eq.split('+')
+    if len(eq) > 1:
+        return solveAdditions(eq)
+    else:
+        eq = eq[0].split('-')
+        if len(eq) > 1:
+            return solveSubtraction(eq)
+        else:
+            eq = eq[0].split('*')
+            if len(eq) > 1:
+                return solveMultiplication(eq)
+            else:
+                eq = eq[0].split('/')
+                if len(eq) > 1:
+                    return solveDivision(eq)
+                else:
+                    return float(str(eq[0]))
 
+
+def solveAdditions(eq):
+    for i in range(len(eq)):
+        eq[i] = processOperations(str(eq[i]))
+    return sum(eq)
+
+def solveSubtraction(eq):
+    for i in range(len(eq)):
+        eq[i] = processOperations(str(eq[i]))
+    return eq[0] - sum(eq[1:])
+
+def solveMultiplication(eq):
+    prod = 1
+    for i in range(len(eq)):
+        eq[i] = processOperations(str(eq[i]))
+        prod *= float(eq[i])
+    return prod
+
+def solveDivision(eq):
+    quoz = processOperations(str(eq[0]))
+    for i in range(1, len(eq)):
+        eq[i] = processOperations(str(eq[i]))
+        quoz /= float(eq[i])
+    return quoz
 
 
 equation = '20 / (5 - 4) / (2 * (12 + 5))'
